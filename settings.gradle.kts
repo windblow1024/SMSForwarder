c259d71c8467600a8f039790abc80bcf5629a47f
windblow1024 <160717168+windblow1024@users.noreply.github.com>
2026-06-29 23:51:12 +0800

    Fix /home/node/.openclaw/workspace/SMSForwarder/settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
