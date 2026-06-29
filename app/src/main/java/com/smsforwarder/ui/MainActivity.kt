3b06eeb2e17b1d35a24861e513bd0cd396124128
windblow1024 <160717168+windblow1024@users.noreply.github.com>
2026-06-30 00:11:35 +0800

    Add missing import for MainScreen in MainActivity

--- app/src/main/java/com/smsforwarder/ui/MainActivity.kt
+++ app/src/main/java/com/smsforwarder/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.smsforwarder.ui.screen.MainScreen
 import com.smsforwarder.ui.theme.SmsForwarderTheme
 
 class MainActivity : ComponentActivity() {
